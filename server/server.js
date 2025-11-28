61c2aae5d2f70d666086e04ebd8ff926c8e15cba
phicae4f <mila.masterova@gmail.com>
2025-11-28 22:49:36 +0300

    Revert "env server"
    This reverts commit 4f4041370889ac75d31a959a0ff2ee64dfacaf26.

--- server/server.js
+++ server/server.js
@@ -3,15 +3,11 @@ const bodyParser = require("body-parser");
 const routes = require("./routes");
 const cors = require("cors")
 
-require("dotenv").config();
-
 const app = express();
 const PORT = 8000;
 
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
-
 app.use(cors({
-  origin: CLIENT_URL,
+  origin: "http://localhost:5173",
   credentials: true
 }))
 
